cc5e71e2e9e46f025eda013b5157cde4cb453514
itggangpae <ggangpae1@gmail.com>
2020-11-26 13:10:52 +0900

    1126

---
+++ javabasic/src/chap18/BorderPaneMain.java
@@ -0,0 +1,22 @@
+package chap18;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BorderPaneMain extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("borderpane.fxml"));
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ javabasic/src/chap18/Box.java
@@ -0,0 +1,23 @@
+package chap18;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Box extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		VBox root = (VBox)FXMLLoader.load(
+			getClass().getResource("box.fxml")
+		);
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
