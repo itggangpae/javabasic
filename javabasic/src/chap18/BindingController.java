fc8f255ba5bc2cf62804b1f38ca582ddd3cda8eb
itggangpae <ggangpae1@gmail.com>
2020-11-30 13:07:38 +0900

    1130

---
+++ javabasic/src/chap18/BindingController.java
@@ -0,0 +1,18 @@
+package chap18;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+public class BindingController implements Initializable {
+	@FXML private TextArea textArea1;
+	@FXML private TextArea textArea2;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		textArea1.textProperty().bind(textArea2.textProperty());
+	}
+}
