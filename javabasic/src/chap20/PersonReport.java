36e36490eeb984b809d055def060ba0db896bcf9
mac <mac@218.38.137.28>
2020-12-21 18:31:08 +0900

    1221

---
+++ javabasic/src/chap20/PersonReport.java
@@ -0,0 +1,21 @@
+package chap20;
+
+import java.util.List;
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "PersonList")
+public class PersonReport {
+	@XmlElement(name = "Person")
+	private List<Person> list;
+	
+	public PersonReport() {
+	}
+	public PersonReport(List<Person> list) {
+		this.list = list;
+	}
+	
+	public List<Person> getList() {
+		return list;
+	}
+}
