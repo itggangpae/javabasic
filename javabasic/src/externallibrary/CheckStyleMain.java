40c146004a8d395ff788f8fbe9af37ca0c586308
itggangpae <ggangpae1@gmail.com>
2021-04-18 11:28:37 +0900

    0418

--- javabasic/src/externallibrary/CheckStyleMain.java
+++ javabasic/src/externallibrary/CheckStyleMain.java
@@ -1,6 +1,10 @@
 package externallibrary;
 
+/**
+ * CheckStyle 적용을 위한 클래스
+ */
 public class CheckStyleMain {
+	
 	public static void main(String[] args) {
 		EmployeeService emp = new EmployeeService();
 		System.out.println(emp);
