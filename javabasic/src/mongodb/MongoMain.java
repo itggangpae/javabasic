cf064ad1c4394dfdfa9063f785a5b671ede3df7c
itggangpae <ggangpae1@gmail.com>
2021-05-28 14:18:54 +0900

    20210528

---
+++ javabasic/src/mongodb/MongoMain.java
@@ -0,0 +1,18 @@
+package mongodb;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoMain {
+	public static void main(String[] args) {
+		//Mongo DB 연결 
+		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+		//test 라는 데이터베이스 연결 
+		MongoDatabase database = mongoClient.getDatabase("mymongo");
+		//소유한 모든 컬렉션 확인 
+		for (String name : database.listCollectionNames()) {
+			System.out.println(name);
+		}
+	}
+}
